7fcc922f0d4fe237c92077a81adf2bc7fbacdf37
Yingjie Lian <670552394@qq.com>
2017-03-21 06:20:16 -0600

    Add files via upload

--- src/assignment06/LogoPanel.java
+++ src/assignment06/LogoPanel.java
@@ -5,39 +5,114 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
+/**
+ * Objects of this class are extended JPanel objects.
+ * Our class adds the functionality of drawing U logo,
+ * changing it color, and responding to GUI elements.
+ * 
+ * @author Yingjie Lian
+ * @version 03/20/2017
+ * @class CS-1410
+ *
+ */
 public class LogoPanel extends JPanel implements ActionListener{
 
+	// This object is the application object that created
+	// this DicePanel object.  This allows us to call 
+	// methods in the application that built us.
 	private UtahLogo enclosingUtahLogo;
 	
+	// This array holds the value in order to change the logo's color
 	private int[] colorChange;
 	
+	/**
+	 * Builds the logo Panel. The caller must in a reference
+	 * to the application that built this panel.
+	 * 
+	 * @param enclosingUtahLogo a reference to the application we're in.
+	 */
 	public LogoPanel(UtahLogo enclosingUtahLogo)
 	{
+		// Keep the reference to the enclosing application so that 
+		// we can call it's functions later.
 		this.enclosingUtahLogo = enclosingUtahLogo;
+		
+		// Initialize the array size
 		colorChange = new int[2];
+		
+		// Show a dialog box to remind the user
+		JOptionPane.showMessageDialog(null, "Thanks for opening my application! You will see a "
+				+ "\nlogo later. Please tell me what letter you just saw "
+				+ "\nand enter the letter into the textfield then click "
+				+ "\n'click' button.");
 		randomize();
+		
 	}
 	
+	/**
+	 * Overrides the JPanel paint method. This method draws the logo
+	 * whenever the GUI system determines that out panel needs to  
+	 * be drawn.
+	 * 
+	 * @param g A graphics object that represents the drawable area
+	 */
 	public void paint (Graphics g)
 	{
+		// Make the background be in white color
 		g.setColor(Color.white);
     	g.fillRect(0, 0, this.getWidth(), this.getHeight());
     	
+    	// Change the color for the U logo
     	for (int i = 0; i < colorChange.length; i++)
-    		drawFlag(g, 200, 200, colorChange[i]);
+    		drawLogo(g, 200, 200, colorChange[i]);
 	}
 	
-	public void drawFlag (Graphics g, int x, int y, int v)
+	/**
+	 * Draw the U logo at x, y.
+	 * 
+	 * @param g the graphics object for our drawable area
+	 * @param x the x coordinate where the logo should be drawn
+	 * @param y the y coordinate where the logo should be drawn
+	 * @param v the value to change the color
+	 */
+	public void drawLogo (Graphics g, int x, int y, int v)
 	{
+		// First fill out a rectangle
 		g.fillRect(0, 0, this.getWidth(), this.getHeight());
-		g.setColor(Color.RED);
+		
+		// use swith-case to change the color of logo.
+		switch (v)
+		{
+		case 0:
+			g.setColor(Color.RED);
+			break;
+		case 1:
+			g.setColor(Color.RED);
+			break;
+		case 2:
+			g.setColor(Color.GREEN);
+			break;
+		case 3:
+			g.setColor(Color.YELLOW);
+			break;
+		case 4:
+			g.setColor(Color.RED);
+			break;
+		case 5:
+			g.setColor(Color.MAGENTA);
+		}
+		// First fill out a rectangle
 		g.fillRect(x, y, 200, 200);
 		
+		// White out the shape that not belongs to
+		// the logo (like a eraser)
 		g.setColor(Color.WHITE);
 		g.fillRect(x+75, y+50, 50, 80);
 		g.fillRect(x+85, y, 30, 50);
@@ -46,6 +121,9 @@ public class LogoPanel extends JPanel implements ActionListener{
 		g.fillRect(x+190, y+50, 10, 150);
 		g.fillRect(x, y+190, 200, 10);
 		
+		// Erase the two little parts at left and right bottom
+		// of the Big red Rectangle to make the logo looks perfectly
+		// like the U logo.
 		for(int i = 0; i < 45; i++)
 		{
 			for(int j = 0; j <= 25; j++)
@@ -55,37 +133,44 @@ public class LogoPanel extends JPanel implements ActionListener{
 			}
 		}
 		
-		switch (v)
-		{
-		case 1:
-			g.setColor(Color.blue);
-			break;
-		case 2:
-			g.setColor(Color.GREEN);
-			break;
-		case 3:
-			g.setColor(Color.YELLOW);
-			break;
-		}
+		
 		
 	}
 	
+	// Use number to control the color of the logo. What I thought
+	// is the only way we can do for changing color is to use number
+	// control it.
 	public void randomize()
 	{
 		for (int i = 0; i < colorChange.length; i++ )
-			colorChange[i] = (int)(Math.random()*3 + 1);
+			colorChange[i] = (int)(Math.random()*7);
 	}
+	
+	/**
+	 * This method implements the required method for receiving 
+	 * action events. (Action events only require one such method.
+	 * Some event types require multiple methods.)
+	 * 
+	 * This method determines the source of the event (GUI, timer,
+	 * button, etc.) and updates the state of the dice and GUI 
+	 * appropriately.
+	 * @param ActionEvent the event that caused this method to be called
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
 		Object source = e.getSource();
 		
+		// If the timer expired, just change the color
+		// of the logo
 		if (source instanceof Timer)
 		{
 			randomize();
 			repaint();
 		}
 		
+		// If the source of the event was the information
+		// button, show user the Basic information of U of U.
 		if (enclosingUtahLogo.isInfoButton(source))
 		{
 			repaint();
@@ -102,11 +187,18 @@ public class LogoPanel extends JPanel implements ActionListener{
 					+ "\nfor an enrollment total of 31,673.");
 		}
 		
+		// If the source of the event was the click
+		// button, show user something.
+		if (enclosingUtahLogo.isClick(source))
+		{
+			enclosingUtahLogo.enterU();
+		}
+			
 	}
 	
 	public Dimension getMinimumSize ()
 	{
-		return new Dimension(660, 660);
+		return new Dimension(520, 520);
 	}
 	public Dimension getPreferredSize ()
 	{
@@ -116,5 +208,4 @@ public class LogoPanel extends JPanel implements ActionListener{
 	{
 		return getMinimumSize();
 	}
-
 }
--- src/assignment06/UtahLogo.java
+++ src/assignment06/UtahLogo.java
@@ -1,77 +1,157 @@
 package assignment06;
 
 import java.awt.BorderLayout;
+
 import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.Timer;
 
 import gui.ApplicationMain;
-
+/**
+ * This application builds a GUI that show the user a 
+ * color-changed logo of University of Utah. The logo 
+ * will change to 4 different colors, but Red will stay
+ * longer because Red is Utah Theme color. Also, when user
+ * press the button and press U button it will show user 
+ * a dialog box.
+ * 
+ * @author Yingjie Lian
+ * @version 03/20/2017
+ * @class CS-1410
+ *
+ */
 public class UtahLogo implements Runnable
 {
-	private JTextField flagBackground;
-	private JButton infoButton;
-	
+	// Instance fields (object variables)
+	private JTextField flagBackground,name;
+	private JLabel enterName;
+	private JButton infoButton, click;
+
 	public static void main(String args[])
 	{
+		// 
 		SwingUtilities.invokeLater(new UtahLogo());
 	}
 	
-	
+	// Instance methods
+	/**
+	 * Builds the GUI for this application. This method sould
+	 * only be called from the FUI thread. (Our main method 
+	 * takes care of this.)
+	 */
 	public void run() 
 	{
-		JFrame frame = new JFrame("Utah Logo");
+		// Build the GUI
+		JFrame frame = new JFrame("Utah Logo"); // Set a name at the top-level window.
 		frame.setVisible(true);
-		frame.setSize(660, 660);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// Make sure when user x out the
 		frame.setBackground(Color.WHITE);
 		
+		// Build some GUI objects
 		flagBackground = new JTextField("text");
 		flagBackground.setColumns(20);
 		flagBackground.setEditable(false);
 		
+		enterName = new JLabel("Enter A Letter: ");
+		name = new JTextField();
+		name.setColumns(5);
+		// Creats a button
 		infoButton = new JButton("Information");
+		click = new JButton("click");
 		
-		JPanel content = new JPanel();
+		// Build all the panels which contain other GUI
+		// objects and set their layout, use these layouts
+		// to arrange all the panel for us.
+		
+		JPanel content = new JPanel(); // The container for the window
 		content.setLayout(new BorderLayout());
 		
-		JPanel topPanel = new JPanel();
-		LogoPanel centerPanel = new LogoPanel(this);
+		JPanel topPanel = new JPanel();// Sub-containers
+		LogoPanel centerPanel = new LogoPanel(this); // Class we created
 		JPanel bottomPanel = new JPanel();
+		JPanel eastPanel = new JPanel();
 		
-		topPanel.setLayout(new FlowLayout());
+		
+		topPanel.setLayout(new FlowLayout()); // Set layout vertically
 		bottomPanel.setLayout(new FlowLayout());
+		eastPanel.setLayout(new FlowLayout());
 		
+		
+		// Listen to events
 		infoButton.addActionListener(centerPanel);
-		
+		click.addActionListener(centerPanel);
+
+		// Add the GUI objects to the panels
 		topPanel.add(new JLabel("University of Utah"));
+		eastPanel.add(enterName);
 		
+		// add button onto the panel
 		bottomPanel.add(infoButton);
+		eastPanel.add(name);
+		eastPanel.add(click);
 		
 		content.add(topPanel, BorderLayout.NORTH);
 		content.add(centerPanel, BorderLayout.CENTER);
 		content.add(bottomPanel, BorderLayout.SOUTH);
+		content.add(eastPanel, BorderLayout.EAST);
 		
-		frame.setContentPane(content);
+		frame.setContentPane(content); // The top-level panel is the content area of the window.
 		
-		frame.pack();
-		frame.setVisible(true);
+		frame.pack();// Calculates the frame's natural size
+		frame.setVisible(true); // Display it.
 		
-		Timer t = new Timer(1000, centerPanel);
-		t.start();
+		// Build a new Timer that triggers every 500 milliseconds (0.5 s)
+		// and let it send ActionEvents to our Panel. Start it.
+		Timer t = new Timer(500, centerPanel);
+		t.start(); // start the timer when application was opened
 		
 	}
 	
+	/**
+	 * Return true if the passed object is our GUI 
+	 * object for U logo.
+	 * 
+	 * @param o any object
+	 * @return true if the object is our GUI information button
+	 */
 	public boolean isInfoButton (Object o)
 	{
 		return o == infoButton;
 	}
 
+	/**
+	 * Return true if the passed object is our GUI 
+	 * object for U logo.
+	 * 
+	 * @param o any object
+	 * @return true if the object is our GUI click button
+	 */
+	public boolean isClick (Object o)
+	{
+		return o == click;
+	}
+	
+	/**
+	 * Check if the user input equals to "u". If yes, show them a dialog box, otherwise
+	 * not.
+	 */
+	public void enterU ()
+	{
+		String s = "u";
+		if(s.equals(name.getText().toLowerCase()))
+				JOptionPane.showMessageDialog(null, "Good job! Go Utes!");
+		
+	}
+	
 }
